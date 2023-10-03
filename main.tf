b0e16dc5308ce34f3134577d16a0319420fed87c
thiagoclessa <thiagoclessa@gmail.com>
2023-10-03 10:56:41 -0300

    teste_auth

--- main.tf
+++ main.tf
@@ -80,3 +80,100 @@ resource "kubectl_manifest" "storageclass" {
 YAML
   depends_on = [module.cluster]
 }
+
+# Access EKS with AWS CLI
+## Create IAM Root User 
+resource "aws_iam_user" "root-user" {
+  name = "root-user"
+
+  depends_on = [
+    module.cluster.aws_eks_cluster.this,
+    module.cluster.aws_eks_node_group.workers
+  ]
+}
+
+resource "aws_iam_access_key" "root-user_key" {
+  user = aws_iam_user.root-user.name
+
+  depends_on = [
+    aws_iam_user.root-user
+  ]
+}
+
+output "root_user_access_key_id" {
+  value     = aws_iam_access_key.root-user_key.id
+  sensitive = true
+}
+
+output "root_user_secret_access_key" {
+  value     = aws_iam_access_key.root-user_key.secret
+  sensitive = true
+}
+
+output "root_user_name" {
+  value = aws_iam_user.root-user.name
+}
+
+output "root_user_arn" {
+  value = aws_iam_user.root-user.arn
+}
+
+## Create IAM Developer User
+
+resource "aws_iam_policy" "developer" {
+  name        = "developer"
+  description = "Política para desenvolvedores com permissões limitadas"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+          "eks:Describe*",
+          "eks:List*",
+          "s3:Get*",
+          "s3:List*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user" "developer" {
+  name = "developer"
+}
+
+resource "aws_iam_access_key" "developer_key" {
+  user = aws_iam_user.developer.name
+}
+
+resource "aws_iam_user_policy_attachment" "developer" {
+  user       = aws_iam_user.developer.name
+  policy_arn = aws_iam_policy.developer.arn
+}
+
+## Apply IAM Users in Cluster Auth ConfigMap
+resource "kubernetes_config_map" "aws_auth" {
+
+  metadata {
+    name      = "root-aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapUsers = jsonencode([{
+      userarn  = aws_iam_user.root-user.arn
+      username = aws_iam_user.root-user.name
+      groups   = ["system:masters"]
+    }])
+    mapRoles = jsonencode([{
+      groups   = ["system:bootstrappers", "system:nodes"],
+      rolearn  = aws_iam_role.workers.arn,
+      username = "system:node:{{EC2PrivateDNSName}}"
+    }])
+  }
+  depends_on = [local_file.kubeconfig]
+}
